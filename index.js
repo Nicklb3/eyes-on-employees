9f20d4978f8518e5001b2e96cacd8dae66e684d3
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-03-07 22:40:30 -0800

    made viewAllDepartments and viewAllRoles functions

--- index.js
+++ index.js
@@ -20,6 +20,58 @@ const options = [
     },
 ];
 
-function init() {
-    askPromptQuestions();
+function askPromptQuestions() {
+    inquirer
+      .prompt(options)
+  
+      // data = giant object (answers from input)
+      .then((answers) => {
+        // console.log(answers);
+        // each of these will be a promise / async await
+        if (answers.optionSelection === "View all departments") {
+          viewAllDepartments();
+        }
+        if (answers.optionSelection === "View all roles") {
+          viewAllRoles();
+        }
+        if (answers.optionSelection === "View all employees") {
+          viewAllEmployees();
+        }
+        if (answers.optionSelection === "Add a department") {
+          addDepartment();
+          // not dependent upon anything else
+        }
+        if (answers.optionSelection === "Add a role") {
+          addRole();
+          // have to give user a selection of dept
+          // dependent upon dept and emp
+        }
+        if (answers.optionSelection === "Add an employee") {
+          addEmployee();
+          // dependent upon role and dept
+        }
+        if (answers.optionSelection === "Update an employee role") {
+          updateEmployeeRole();
+   
+        }
+        if (answers.optionSelection === "Start over") {
+          return askPromptQuestions();
+        }
+    });
+}
+
+function viewAllDepartments () {
+    Queries.viewAllDepartments().then(([rows]) => {
+        let departments = rows;
+        console.table(departments);
+        askPromptQuestions();
+    });
+}
+
+function viewAllRoles () {
+    Queries.viewAllRoles().then(([rows]) => {
+        let roles = rows;
+        console.table(roles);
+        askPromptQuestions();
+    });
 }
