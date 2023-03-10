a8c4ec5eba424cfd2e30e23e7afddb23e8b4c630
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-03-09 18:18:23 -0800

    commented code

--- db/schema.sql
+++ db/schema.sql
@@ -3,11 +3,19 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
+-- department
+-- id: INT PRIMARY KEY
+-- name: VARCHAR(30) to hold department name
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
 );
 
+-- role
+-- id: INT PRIMARY KEY
+-- title: VARCHAR(30) to hold role title
+-- salary: DECIMAL to hold role salary
+-- department_id: INT to hold reference to department role belongs to
 CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(100),
@@ -18,6 +26,12 @@ CREATE TABLE role (
   ON DELETE SET NULL
 );
 
+-- employee
+-- id: INT PRIMARY KEY
+-- first_name: VARCHAR(30) to hold employee first name
+-- last_name: VARCHAR(30) to hold employee last name
+-- role_id: INT to hold reference to employee role
+-- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
